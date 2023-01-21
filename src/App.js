6474fffaf0317a1b486bec01bdd3318cef7228a8
priyasu-cx <codexposer.priyasu25@gmail.com>
2023-01-21 13:48:43 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./global_components/navbar";
+import { Routes, Route } from "react-router-dom";
+import Hero from "./pages/hero";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      {/* <Routes>
+        <Route path="/" element={<Hero />} />
+      </Routes> */}
+    </>
   );
 }
 
---
+++ src/pages/hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+  return (
+    //create an event
+    <div className="my-10">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Add Event
+    </button>
+    </div>
+    
+  );
+}
+export default Hero;
