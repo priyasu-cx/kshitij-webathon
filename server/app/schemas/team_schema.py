c62f1b5b26d744b9e0c5bb243ff958a0e500bfe5
shubhayu-64 <majumdarshubhayu@gmail.com>
2023-01-22 15:23:12 +0530

    zz

--- server/app/schemas/team_schema.py
+++ server/app/schemas/team_schema.py
@@ -8,3 +8,7 @@ class TeamDBSchema(BaseModel):
     teamDescription: str
     teamLeadID: str
     teamMembers: List[str] = Field(min_length=1, max_length=10)
+    eventName: str
+    eventDescription: str
+    eventDate: str
+    
